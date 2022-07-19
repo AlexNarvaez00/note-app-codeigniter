b6897fe0df8bb5403717ab38073dc0a677a3aecb
ELNarvaez1 <zaizealexis@gmail.com>
2022-07-19 00:46:28 -0500

    Cambios

--- app/Controllers/Notes.php
+++ app/Controllers/Notes.php
@@ -10,8 +10,9 @@ class Notes extends BaseController
 	public function index()
 	{
 		$views = __DIR__.'/../Views';
+		$cache = __DIR__.'/../../vendor/cache';
 
-		$bladeObj = new PhpBlade($views,null);
-		return $bladeObj->view()->make('home');
+		$bladeObj = new PhpBlade($views,$cache);
+		echo $bladeObj->view()->make('home');
 	}
 }
--- app/Views/home.blade.php
+++ app/Views/home.blade.php
@@ -1,6 +1,4 @@
-@extends('components.root')
-@section('main-content')
 <p>
-	Contenido extra
+Amet amet nobis nisi consequuntur perferendis totam! Cupiditate explicabo magnam sapiente aliquid aperiam! Rerum impedit quod illo amet exercitationem Obcaecati sint magni blanditiis reprehenderit sed. Totam laboriosam praesentium necessitatibus quisquam!
+
 </p>
-@endsection
