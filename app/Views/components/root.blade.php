761ed1738cd7683e4d75b8dd02b7b249e1d5b944
ELNarvaez1 <zaizealexis@gmail.com>
2022-07-19 00:59:18 -0500

    Agregamos blade pero algunas directivas puede no reconocerlas

--- app/Views/components/root.blade.php
+++ app/Views/components/root.blade.php
@@ -1,18 +1,20 @@
+<?= helper('html') ?>
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
-	<title></title>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+ <?= link_tag('cork/src/bootstrap/css/bootstrap.min.css') ?>
+ <title>Titulo para todas las plantillas</title>
 </head>
 
 <body>
-	<p>
-		Esta es la prueba de fuego para poder usar blade
-	</p>
-	@yield('main-content)
+ <p>
+  Esta es la prueba de fuego para poder usar blade
+ </p>
+ @yield('main-content')
 </body>
 
 </html>
--- app/Views/home.blade.php
+++ app/Views/home.blade.php
@@ -1,4 +1,6 @@
+@extends('components.root')
+@section('main-content')
 <p>
-Amet amet nobis nisi consequuntur perferendis totam! Cupiditate explicabo magnam sapiente aliquid aperiam! Rerum impedit quod illo amet exercitationem Obcaecati sint magni blanditiis reprehenderit sed. Totam laboriosam praesentium necessitatibus quisquam!
-
+	Contenido extra sdcjnskcdkscdnksjndc
 </p>
+@endsection
